a657b44b0fc8ab98509db6b66a74c74202f9dbbd
jayshreedwagh <jayshreepatil5930@gmail.com>
2025-02-25 20:03:28 +0530

    image stored in cloudinary

---
+++ cloudinary.js
@@ -0,0 +1,10 @@
+const cloudinary = require("cloudinary").v2;
+
+// Configure Cloudinary with your credentials
+cloudinary.config({
+  cloud_name: "dgmho4nox",
+  api_key: "788945851416951",
+  api_secret: "agdeLh4QxIytS0iRk0eQ-vtiacs",
+});
+
+module.exports = cloudinary;
